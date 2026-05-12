22b2ede78f5ff6c93b99fab8a88f5f1fa55c8777
abwmwry222-spec <abwmwry222@gmail.com>
2026-05-12 22:37:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,3 @@
 import 'package:flutter/material.dart';
-import 'ymai.dart'; // ربط الملف الثاني
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'YMAI App',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const YmaiPage(), 
-    );
-  }
-}
+void main() => runApp(const MaterialApp(home: Scaffold(body: Center(child: Text('YMAI App Ready')))));
