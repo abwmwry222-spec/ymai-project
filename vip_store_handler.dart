728aaa50775e56556045036a140946426ecf6b19
abwmwry222-spec <abwmwry222@gmail.com>
2026-05-19 22:25:45 +0300

    Update vip_store_handler.dart

--- vip_store_handler.dart
+++ vip_store_handler.dart
@@ -3,11 +3,10 @@ class VipStoreHandler {
 
   bool get isVip => _isUserVip;
 
-  /// فحص صلاحيات الوصول للميزات المتقدمة
   bool hasAccessToFeature(String featureName) {
     List<String> vipExclusiveFeatures = [
-      'export_4k',        // جودة 4K حصرية
-      'remove_watermark', // إزالة الشعار حصري
+      'export_4k',        
+      'remove_watermark', 
     ];
 
     if (vipExclusiveFeatures.contains(featureName) && !_isUserVip) {
@@ -16,7 +15,6 @@ class VipStoreHandler {
     return true; 
   }
 
-  /// تفعيل أو إلغاء الـ VIP
   void toggleVipStatus() {
     _isUserVip = !_isUserVip;
   }
